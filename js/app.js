0db97df723703bed76566e56e5b22e7d001b5736
devspotion <jovanovicbojandev@gmail.com>
2021-04-21 21:55:20 +0200

    Created sidebar navigation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
+// DOM ELements
+const navToggler = document.getElementById('sidebar-toggler');
+const sidebar = document.querySelector('.sidebar');
+
+// Varibales
 const songs = getSongs();
+console.log(songs);
+
+// Functions
+
+// Event Listeners
+navToggler.addEventListener('click', e => {
+  e.preventDefault();
+
+  let toggler = navToggler.querySelector('i');
+  sidebar.classList.toggle('extend');
+
+  toggler.className = toggler.className.includes('off') ? toggler.className.replace('off', 'on') : toggler.className.replace('on', 'off');
+});
